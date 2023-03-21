513e05bd5ee989412bef17ba1f478b090c5451ec
Daniel Campos <daniel.campostello@gmail.com>
2023-03-21 01:23:28 +0100

    Update Startup.cs

--- TasteItApi/Startup.cs
+++ TasteItApi/Startup.cs
@@ -34,7 +34,7 @@ namespace TasteItApi
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TasteIt", Version = "v1" });
             });
 
-            var client = new BoltGraphClient(new Uri("neo4j+s://dc95b24b.databases.neo4j.io"), "neo4j", "sBQ6Fj2oXaFltjizpmTDhyEO9GDiqGM1rG-zelf17kg");
+            var client = new BoltGraphClient(new Uri("neo4j+s://BLABLABLA.databases.neo4j.io"), "neo4j", "BLABLABLA");
             client.ConnectAsync();
             services.AddSingleton<IGraphClient>(client);
         }
