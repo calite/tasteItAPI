bb0954e58ae94787fef35b28b7dfdbccfd8d4b1b
Practicas Soporte <practicas.soporte@icp.es>
2023-03-29 12:43:45 +0200

    reparado

--- TasteItApi/Startup.cs
+++ TasteItApi/Startup.cs
@@ -33,7 +33,9 @@ namespace TasteItApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TasteIt", Version = "v1" });
             });
-
+            //NEO
+            //new Uri("neo4j+s://dc95b24b.databases.neo4j.io"), "neo4j", "sBQ6Fj2oXaFltjizpmTDhyEO9GDiqGM1rG-zelf17kg")
+            //new Uri(Configuration["NEO_URL"]), Configuration["NEO_USER"], Configuration["NEO_PASSWORD"]
             var client = new BoltGraphClient(new Uri("neo4j+s://dc95b24b.databases.neo4j.io"), "neo4j", "sBQ6Fj2oXaFltjizpmTDhyEO9GDiqGM1rG-zelf17kg");
             client.ConnectAsync();
             services.AddSingleton<IGraphClient>(client);
